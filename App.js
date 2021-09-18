e367c82a830baefd3a603ef4056a1265010aa210
Alejandro Garcia Gallardo <77563527+AlejandroGaGa@users.noreply.github.com>
2021-09-17 21:48:29 -0500

    Primer commit

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// aca inician los imports
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
+// aca es la estructura de react
 export default function App() {
+  const [modal, setmodal] = useState(false);
+  const [string, setstring] = useState("");
+
+  function changestate() {
+    if (modal != false) {
+      console.log(" es verdadero");
+    }else
+    {
+      console.log(" es falso");
+      setstring("Perro")
+      console.log(string);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>{string}</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View>
+        <Button
+          onPress={() => changestate()}
+          title="Presionar"
+          color="#841584"
+        />
+      </View>
+    </>
   );
 }
 
+// aca se crean los estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#E32636",
+    fontSize: 25,
   },
 });
