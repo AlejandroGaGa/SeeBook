4067c2c8865842f0d07a571ee314ad845b7c0a77
NoeOz <ploz2767@gmail.com>
2021-09-21 17:44:29 -0500

    Side menu navigation

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { View,Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
+//NAVIGATION
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+//AWS
 import Amplify from "aws-amplify";
 import awsconfig from "./src/aws-exports";
 import { AmplifySignOut, withAuthenticator } from "aws-amplify-react-native";
 Amplify.configure(awsconfig);
-// aca es la estructura de react
+
+const Drawer = createDrawerNavigator();
+
+//FUNCTIONS
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View
+        style={{ alignContent: "center", marginLeft: "10%", marginTop: "30%" }}
+      >
+        {/*  <AmplifySignOut />
+      <StatusBar style="auto" /> */}
+        <Text>Bienvenido a SeeBook !</Text>
+      </View>
+      {/* <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      /> */}
+    </View>
+  );
+}
+
+function InterestScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Por el momento no tienes intereses registrados </Text>
+      <Button
+        onPress={() => navigation.goBack()}
+        title="Go back home"
+      />
+    </View>
+  );
+}
+
+//STRUCTURE VIEW REACT NATIVE
 function App() {
   return (
-    <View style={{alignContent:"center", marginLeft:"10%",marginTop:"30%"}}>
-     {/*  <AmplifySignOut />
-      <StatusBar style="auto" /> */}
-      <Text>Hola, ya estas dentro de la app</Text>
-
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Inicio" component={HomeScreen} />
+        <Drawer.Screen name="Intereses" component={InterestScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
