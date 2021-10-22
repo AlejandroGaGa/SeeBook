909745032895443dcf3e4bff61a58a826fc4607f
alex-g-g <alejandro.g.g@track.design>
2021-10-22 12:29:03 -0500

    Consumo de Api de C#

--- components/openlibra.js
+++ components/openlibra.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Openlibra extends Component {
-
   getcategories = () =>
     new Promise(async (resolve, reject) => {
       try {
@@ -10,7 +9,7 @@ class Openlibra extends Component {
           "https://www.etnassoft.com/api/v1/get/?get_categories=all",
           {
             method: "GET",
-            
+
             headers: {
               "Content-Type": "application/json",
             },
@@ -22,15 +21,52 @@ class Openlibra extends Component {
         reject(error);
       }
     });
-    //obtener libros topic 
-    getbookstop = () =>
+  //obtener libros topic
+  getbookstop = () =>
     new Promise(async (resolve, reject) => {
       try {
         const GetBooks = await fetch(
           "https://www.etnassoft.com/api/v1/get/?publisher_date=2019",
           {
             method: "GET",
-            
+
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const getbookstop = await GetBooks.json();
+        resolve(getbookstop);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  //obtener libros topic
+  listbd = () =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const GetBooks = await fetch(
+          "http://192.168.15.170:7010/api/Producto/ListProducts",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const getbookstop = await GetBooks.json();
+        resolve(getbookstop);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  showid = (id) =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const GetBooks = await fetch(
+          "http://192.168.15.170:7010/api/Producto/Search/" + id + "",
+          {
+            method: "GET",
             headers: {
               "Content-Type": "application/json",
             },
--- screens/subcategories.js
+++ screens/subcategories.js
@@ -1,14 +1,85 @@
 import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, TextInput, StyleSheet, Button } from "react-native";
 import { Auth } from "aws-amplify";
+import Openlibra from "../components/openlibra";
 
 const Subcategoria = (props) => {
-  console.log(props);
+  const [number, onChangeNumber] = React.useState(null);
+
+  useEffect(() => {
+    pet();
+  }, []);
+  const [arrayglo, setarray] = useState([]);
+  const [array2, setar] = useState([]);
+
+  function pet() {
+    var array = [];
+    let Open = new Openlibra();
+    Open.listbd()
+      .then((result) => {
+        for (var i in result) {
+          array.push(result[i]);
+        }
+        setarray(array);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function petbyid(int) {
+    let Open = new Openlibra();
+    Open.showid(int)
+      .then((result) => {
+        setar(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  console.log("-------3----------->", array2);
+  /*   console.log("Este es el array", arrayglo); */
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Mis Categorias</Text>
+      {arrayglo.map((item, i) => (
+        <Text>
+          {item.Id_Producto}.-{item.Nombre_Producto}
+        </Text>
+      ))}
+      <Text style={{ fontSize: 15, marginTop: 10 }}>
+        Ingrese un id para mostrar los datos de ese usuario
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+      />
+      <Button
+        onPress={() => petbyid(number)}
+        title="Mostrar datos"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      {array2.map((item, i) => (
+        <>
+          <Text>
+            {item.Id_Producto}.-{item.Nombre_Producto}
+          </Text>
+          <Text>
+           Su rfc es: {item.rfc} y su nikname es: {item.nikname}
+          </Text>
+        </>
+      ))}
     </View>
   );
 };
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default Subcategoria;
